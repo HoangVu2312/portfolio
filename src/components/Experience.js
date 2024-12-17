793238374c8d7bcd11dc101dd7098377cee8c43d
HoangVu2312 <103991547+HoangVu2312@users.noreply.github.com>
2024-12-17 12:46:03 +0200

    Update more Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -39,12 +39,13 @@ const Experience = () => {
             <CircleIcon reference={Iref} />
 
             <div>
-              <h3>University of Technology and Education</h3>
-              <span>2017-2019 | HCM city, VietNam</span>
+              <h3>LAB University of Applied Sciences</h3>
+              <span>2021-2024 | Lahti, Finland</span>
               <p>
-                I stared my higher education in VietNam with the major
-                Autiomation Technology. I learned mostly about car-automation
-                and the logic behind it.
+                Choosing Information Technology was the right choice for me when
+                i started university in Finland. I learned most of the basic and
+                foundation in software development from my school and I got a
+                chance to develop out my skills with many subjects.
               </p>
             </div>
           </li>
@@ -56,13 +57,15 @@ const Experience = () => {
             <CircleIcon reference={Iref2} />
 
             <div>
-              <h3>LAB University of Applied Sciences</h3>
-              <span>2020-now | Lahti, Finland</span>
+              <h3>cybersoft Academy</h3>
+              <span>2021-2023 | Coding BootCamp</span>
               <p>
-                Choosing Information Technology was the right choice for me when
-                i started university in Finland. I learned most of the basic and
-                foundation in software development from my school and I got a
-                chance to test out my skills with many subjects.
+                Aside from University, I also undertook an intense training
+                course at Cybersoft Academy where I learned the advanced concept
+                and got a profound understanding of this industry. Additionally,
+                for the last 6 months, I have had some experience working with a
+                two start-up companies as an exam to graduate from this
+                Academy.
               </p>
             </div>
           </li>
@@ -74,15 +77,15 @@ const Experience = () => {
             <CircleIcon reference={Iref3} />
 
             <div>
-              <h3>cybersoft Academy</h3>
-              <span>2021-2023 | HCM city, VietNam</span>
+              <h3>Studifahrten</h3>
+              <span>2023 | Cologne, Germany</span>
               <p>
-                Aside from University, I also undertook an intense traning
-                course at Cybersoft Academy where I learned the advance concept
-                and got a profound understanding of this industry. Additionally,
-                for the last 6 months, I had some experience working with a
-                couple of start-up companies as an exam to graduate from this
-                Academy
+                I got a chance to have an internship in Germany thanks to the
+                Erasmus program at my University. During 4 months here, I have
+                learn about a wide range of skills and knowledge related to
+                UX/UI development. I have gained a valuable experience with
+                various UX/UI designing tools and technologies used in the
+                industry.
               </p>
             </div>
           </li>
@@ -95,12 +98,14 @@ const Experience = () => {
             <CircleIcon reference={Iref4} />
 
             <div>
-              <h3>Teaching Assistant at Cybersoft Academy</h3>
-              <span>2023 | Online, VietNam</span>
+              <h3>Jutice4.0</h3>
+              <span>2024 | Helsinki, Finland</span>
               <p>
-                After graduation, I decided to stay to support my teacher as an
-                Assistant. My job was mostly to answer questions from students
-                and helped them understand about the lessons better.
+                At the beginning of 2024, I had a chance to work as a front-end developer
+                at Justice4.0, a start-up company based in Helsinki, Finland. Here is where 
+                I learn to work with different people on building the Jutice Lady application. 
+                My tasks were using React and Typescript to build dynamic user interfaces and
+                solve different technical issues.  
               </p>
             </div>
           </li>
@@ -113,15 +118,14 @@ const Experience = () => {
             <CircleIcon reference={Iref5} />
 
             <div>
-              <h3>Studifahrten Gmbh-Internship</h3>
-              <span>2023| Cologne, Germany</span>
+              <h3>Kraftvaerk</h3>
+              <span>Jul 2024-Now | Helsinki, Finland</span>
               <p>
-                I got a chance to have an internship in Germany thanks the
-                Eramus program at my University. During 4 months here, I have
-                learn about a wide range of skills and knowledge related to
-                UX/UI development. I have gained a valuable experience with
-                various UX/UI designing tools and technologies used in the
-                industry.
+                After graduation, I also had another opportunity at Kraftvaerk, a digital service company,
+                where I learned to become a proficient developer. As a Frontend Developer, my main tasks involve implementing UI components 
+                and interfaces using React and NextJS, integrating CMS platforms like Optimizely to modify and deliver dynamic content, and
+                optimizing web performance.  I have also played a part in collaborating with the stakeholders, using cloud services like 
+                Azure and AWS, to translate customer’s requirements into scalable and maintainable web applications.
               </p>
             </div>
           </li>
